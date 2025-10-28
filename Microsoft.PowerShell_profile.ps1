24b4d5fa40459f093ba44a61604f46b793f453c6
tkuwill <billwang1023@gmail.com>
2025-10-29 02:58:47 +0800

    Add zoxide's config into $PROFILE.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,15 @@
+# PowerShell Configuration
+
+# module Configuration
 Set-Alias -Name 'vim' -Value 'C:\Program Files\Notepad++\notepad++.exe'
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd 
 Set-PSReadLineOption -MaximumHistoryCount 60000
 Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 
+$ENV:STARSHIP_CONFIG = "$HOME\starship\starship.toml"
+# zoxide starting
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
-$ENV:STARSHIP_CONFIG = "$HOME\starship\starship.toml"
+# prompt Configuration
 Invoke-Expression (&starship init powershell)
